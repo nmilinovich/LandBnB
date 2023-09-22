97322bb4b44836efc070fa51e465e944a82722a1
Nikola Milinovich <nmilinovich1@gmail.com>
2023-09-21 20:44:52 -0700

    added userlogin backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 // router.get('/set-token-cookie', async (_req, res) => {
 //     const user = await User.findOne({
