e95e1b295225077ff48c1a61e29abee8681c46e8
Nikola Milinovich <nikolamilinovich1@gmail.com>
2023-10-08 20:07:25 -0700

    closer

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -184,8 +184,6 @@ router.get(
 
         let query = {
             where: {
-                page: page,
-                size: size,
                 // lat: 
             },
             include: [{
@@ -197,12 +195,12 @@ router.get(
                 where: 'preview' === true,
                 attributes: []
             }],
-            attributes: {
-                include: [
-                    [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating'],
-                    [sequelize.col('SpotImages.url'), 'previewImage']
-                ]
-            },
+            // attributes: {
+            //     include: [
+            //         [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating'],
+            //         [sequelize.col('SpotImages.url'), 'previewImage']
+            //     ]
+            // },
         };
         if (
             (!Number.isInteger(page) || page > 10 || page < 1) ||
@@ -228,10 +226,30 @@ router.get(
             };
             err.status = 400;
             return next(err);
-        } else {
-            query.limit = size;
-            query.offset = size * (page - 1);
-        }
+        } 
+
+        if (minLat && maxLat) {
+            query.where.lat = {
+                [Op.gte]: minLat,
+                [Op.lte]: maxLat,
+            };
+        };
+        if (minLng && maxLng) {
+            query.where.lng = {
+                [Op.gte]: minLng,
+                [Op.lte]: maxLng,
+            };
+        };
+        query.limit = size;
+        query.offset = size * (page - 1);
+
+        if (minPrice && maxPrice) {
+            query.where.price = {
+                [Op.gte]: minPrice,
+                [Op.lte]: maxPrice
+            }
+        };
+
 
 
         // const Spots = await Spot.findAll({
@@ -254,11 +272,11 @@ router.get(
 
         console.log(query)
 
-        const Spots = await Spot.findAll({...query});
+        const Spots = await Spot.findAll(query);
 
 
         
-        return res.json({ Spots })
+        return res.json({ Spots, "page": page, "size": size })
     }
 );
 
