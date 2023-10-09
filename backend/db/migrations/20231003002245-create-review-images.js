816984a1d7ea161e511b2fdb1e35a501b6ce6120
Nikola Milinovich <nikolamilinovich1@gmail.com>
2023-10-09 01:04:53 -0700

    fixed no options in 2 migrations

--- backend/db/migrations/20231003002245-create-review-images.js
+++ backend/db/migrations/20231003002245-create-review-images.js
@@ -1,10 +1,13 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
 
 let options = {};
 if(process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 };
+
+/** @type {import('sequelize-cli').Migration} */
+
+
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -33,7 +36,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "ReviewImages"
