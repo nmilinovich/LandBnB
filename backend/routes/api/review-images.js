529a598c10dda340ef21fa785e14a6b768d357c5
Nikola Milinovich <nikolamilinovich1@gmail.com>
2023-10-08 17:11:36 -0700

    finished spotImage deletion

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const { Spot, Review, SpotImages, sequelize, User, Sequelize } = require('../../db/models')
+const { Spot, Review, SpotImages, ReviewImages, sequelize, User, Sequelize } = require('../../db/models')
 const { requireAuth, sendAuthorizationError } = require('../../utils/auth.js')
 const { Op } = require('sequelize');
 const router = express.Router();
 
+
+router.delete(
+    '/:imageId',
+    requireAuth,
+    async (req, res, next) => {
+        const userId = req.user.id;
+        const imageId = req.params.imageId
+
+        const reviewImage = await ReviewImages.findbyPk({
+            where: {
+                userId: userId
+            },
+            include: {
+                model: Review,
+            }
+        });
+
+    }
+);
 
 
 module.exports = router;
--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Spot, Review, SpotImages, sequelize, User, Sequelize } = require('../../db/models')
+const { Spot, Review, ReviewImages, SpotImages, sequelize, User, Sequelize } = require('../../db/models')
 const { requireAuth, sendAuthorizationError } = require('../../utils/auth.js')
 const { Op } = require('sequelize');
 const router = express.Router();
@@ -9,17 +9,29 @@ router.delete(
     requireAuth,
     async (req, res, next) => {
         const userId = req.user.id;
+        const imageId = req.params.imageId
 
-        const reviewImage = await ReviewImage.findbyPk({
-            where: {
-                userId: userId
-            },
+        const spotImage = await SpotImages.findByPk(imageId, {
             include: {
-                model: Review,
+                model: Spot,
+                attributes: ['ownerId']
             }
         });
 
+        console.log("###", spotImage.Spot.ownerId)
+
+        if(spotImage.Spot.ownerId !== userId) {
+            const err = new Error("Forbidden");
+            err.title = "Forbidden";
+            err.errors = "Forbidden";
+            err.status = 403;
+            return next(err);
+        } else {
+            await spotImage.destroy();
+            res.json({ "message": "Successfully deleted" })
+        }
+        
     }
-)
+);
 
 module.exports = router;
